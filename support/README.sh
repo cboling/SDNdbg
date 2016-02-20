9f6a185e01009fcf0d63eca989634caa3756b63a
cboling <Painter456>
2016-02-20 09:34:35 -0600

    Added info on cytoscape.js source

---
+++ support/README.sh
@@ -0,0 +1,16 @@
+This directory contains support libraries that may be used.  For ones available
+on github, I have only included the 'README' from that file.  Below are then
+commands and version info for each of these.
+
+
+|   Directory   | Version | Notes                                            |
+|:-------------:|:-------:| ------------------------------------------------ |
+| cytoscape.js  | ~2.6.4  | Fecthed on 2/20/2016 with the following archive: |
+|               |         | https://github.com/cytoscape/cytoscape.js.git    |
+|               |         | comments indicated about to publish v2.6.4       |
+|               |         |        |
+|               |         |        |
+|               |         |        |
+|               |         |        |
+|               |         |        |
+|               |         |        |
