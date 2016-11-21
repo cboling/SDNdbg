76c29785fc12ae2788b79d8495f19acb7c43b9c7
cboling <Painter456>
2016-11-21 15:03:50 -0600

    missing  files from this weekends work

---
+++ core/controller.py
@@ -0,0 +1,62 @@
+"""
+Copyright (c) 2015 - 2016.  Boling Consulting Solutions , BCSW.net
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+from node import Node
+import pprint
+import logging
+from credentials import Credentials
+
+
+class Controller(Node):
+    """
+    Base class for an NFV/VIM or SDN Contoller
+    """
+    def __init__(self, **kwargs):
+        logging.info('core.Controller.__init__: {}'.format(pprint.PrettyPrinter().pformat(kwargs)))
+
+        Node.__init__(self, **kwargs)
+
+        # Save off site SSH credentials.  Should be useful for most all controller types
+        # TODO: Do we want to support per-VIM/SDN sets of credentials?
+        # TODO: How about global SSH user/passwords to try as well?
+
+        self.ssh_credentials = None
+        config = kwargs.get('config')
+        while config is not None:
+            if config.type.lower() == 'site':
+                self.ssh_credentials = Credentials(config.ssh_username_and_passwords)
+                break
+            config = config.config_parent
+
+    @staticmethod
+    def create(parent, **kwargs):
+        from openstack.controller import Controller as OpenStackController
+        from onos.controller import Controller as ONOSController
+
+        _supported_vims = {
+            'openstack': OpenStackController.create,
+            'onos': ONOSController.create
+        }
+        logging.info('core.Controller.create: {}'.format(pprint.PrettyPrinter().pformat(kwargs)))
+
+        config = kwargs.get('config')
+        create_method = _supported_vims.get(config.type.lower())
+
+        if create_method is None:
+            raise NotImplemented("Controller type '{}' is not supported".format(config.type))
+
+        kwargs['parent'] = parent
+
+        return create_method(**kwargs)
---
+++ openstack/controller.py
@@ -0,0 +1,122 @@
+"""
+Copyright (c) 2015 - 2016.  Boling Consulting Solutions , BCSW.net
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+from core.controller import Controller as CoreController
+from config import Config
+import pprint
+import logging
+from credentials import Credentials
+from keystoneclient.exceptions import ConnectionError, ConnectionRefused, Unauthorized
+
+_services_of_interest = [
+    {'type': 'identity', 'names': ['keystone']},
+    {'type': 'compute', 'names': ['nova']},
+    {'type': 'orchestration', 'names': ['heat', 'tacker']},
+    {'type': 'network', 'names': ['neutron']}
+]
+# TODO: Look into following services: heat-cfg, nova_legacy
+# TODO: Any need to support the following: glance, cinder, switch, ceilometer, ...
+
+
+class Controller(CoreController):
+    def __init__(self, **kwargs):
+        logging.info('openstack.Controller.__init__: entry:\n{}'.format(pprint.PrettyPrinter().pformat(kwargs)))
+
+        CoreController.__init__(self, **kwargs)
+
+    @staticmethod
+    def create(**kwargs):
+        logging.info('openstack.Controller.Create: entry:\n{}'.format(pprint.PrettyPrinter().pformat(kwargs)))
+        return Controller(**kwargs)
+
+    @property
+    def ssh_address(self):
+        """
+        Get address/target for SSH commands
+
+        :return: (String) IP or hostname
+        """
+        return self.config.get_address()
+
+    @property
+    def credentials(self):
+        """
+        Get OpenStack Credentials object
+
+        :return: (Credentials) OpenStack admin access credentials
+        """
+        return self.config.to_credentials()
+
+    def connect(self):
+        """
+        Create credentials for accessing and OpenStack Controller.  A variety of clients are used to work with
+        OpenStack but at this level, we mainly need to enumerate all the services (and endpoints) that we
+        will be acessing.  This requires a Keystone Client
+
+        :return: (KeystoneCLient) Keystone Client
+        """
+        return self.credentials.keystone_client
+
+    def perform_sync(self):
+        """
+        A controller is made up of one or more machines running services that we care about.  There are
+        a large number of OpenStack services, but we currently only care about a few.
+
+        :return: True if synchronization was successful, False otherwise
+        """
+        if self.client is None:
+            return False
+
+        servers = self.get_openstack_servers()
+
+        return False
+
+    def get_openstack_servers(self):
+        keystone_client = self.client
+
+        # Bypass 'subjectAltName' warning.  See https://github.com/shazow/urllib3/issues/497 for more detail
+        import warnings
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            try:
+                services = keystone_client.services.list()
+                endpoints = keystone_client.endpoints.list()
+
+            except ConnectionError as ex:
+                logging.warning('OpenStack.get_openstack_servers[{}]: Connection Error: {}'.format(self, ex.message))
+                return None
+
+            except ConnectionRefused as ex:
+                logging.warning('OpenStack.get_openstack_servers[{}]: Connection Refused: {}'.format(self, ex.message))
+                return None
+
+            except Unauthorized as ex:
+                logging.warning('OpenStack.get_openstack_servers[{} - {}/{}]: Unauthorized: {}'.
+                                format(self, self.config.username, self.config.password, ex.message))
+                return None
+
+            except Exception as e:
+                logging.exception('Error accessing keystone endpoint services')
+                return None
+
+        for service in services:
+            logging.info('  Service: {}'.format(pprint.PrettyPrinter().pformat(service.to_dict())))
+
+        for endpoint in endpoints:
+            logging.info('  Endpoint: {}'.format(pprint.PrettyPrinter().pformat(endpoint.to_dict())))
+
+        # Collect information on services of interest
+
+        raise NotImplementedError('TODO: Implement this')
