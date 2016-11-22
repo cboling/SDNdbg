8c96b09cc0928eda8e07a96062566d1045952522
cboling <Painter456>
2016-11-21 18:25:29 -0600

    unicode for core modules

--- core/controller.py
+++ core/controller.py
@@ -13,10 +13,13 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-from node import Node
-import pprint
+from __future__ import unicode_literals
+
 import logging
+import pprint
+
 from credentials import Credentials
+from node import Node
 
 
 class Controller(Node):
