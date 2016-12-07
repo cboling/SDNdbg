94997868ceca6f84451d64478e04232030128b5e
Chip Boling <chip@bcsw.net>
2016-12-07 13:44:23 -0600

    Added some bonding functionality to the discovery process

--- linux/sys_class_net.py
+++ linux/sys_class_net.py
@@ -21,6 +21,18 @@ import pprint
 
 from client import Client
 
+DEFAULT_SYSFS_MOUNT_POINT = '/sys'
+_sysfs_mount_point = DEFAULT_SYSFS_MOUNT_POINT
+
+
+def set_sysfs_mount_point(point):
+    # TODO: Support alternate mount points within config file
+    _sysfs_mount_point = point
+
+
+def get_sysfs_mount_point():
+    return _sysfs_mount_point
+
 
 def get_sys_class_net_interface_devices(connection, cmd_prefix=""):
     # Walk the /sys/class/net structure and create a dictionary with each device we care about
@@ -36,7 +48,7 @@ def _get_device_list(connection, cmd_prefix=""):
     :return: (list of dict) List of device dictionaries based of /sys/class/net/<device> contents
     """
     ignore = ['lo', 'ovs-system']
-    command = cmd_prefix + '/bin/ls /sys/class/net'
+    command = cmd_prefix + '/bin/ls ' + _sysfs_mount_point + '/class/net'
 
     devices = {}
     device_path = {}
@@ -47,7 +59,7 @@ def _get_device_list(connection, cmd_prefix=""):
         for line in output.split(str('\n')):
             for dev in str.split(line):
                 if dev not in ignore:
-                    device_path[dev] = str('/sys/class/net/{}'.format(dev))
+                    device_path[dev] = str('{}/class/net/{}'.format(_sysfs_mount_point, dev))
 
     except Exception as e:
         logging.exception('Client._get_sys_class_net_interface_devices')
@@ -55,7 +67,10 @@ def _get_device_list(connection, cmd_prefix=""):
     # Walk each device subdirectory a dictionary of information for the device
 
     for name, sys_path in device_path.items():
-        devices[name] = _get_device_detail(connection, sys_path, cmd_prefix)
+        if sys_path == str(_sysfs_mount_point + '/class/net/bonding_master'):
+            devices[name] = _get_bonding_master_detail(connection, sys_path, cmd_prefix)
+        else:
+            devices[name] = _get_device_detail(connection, sys_path, cmd_prefix)
         devices[name][str('name')] = name
         #
         # TODO: Add driver detail and peer information if we can !!!
@@ -66,6 +81,85 @@ def _get_device_list(connection, cmd_prefix=""):
     logging.debug('_get_device_list: output: {}'.format(pprint.PrettyPrinter(indent=2).pformat(devices)))
 
     return devices
+
+
+def _get_bonding_master_detail(connection, sys_path, cmd_prefix=""):
+    """
+    The /sys/class/net/bonding_masters (Sysfs) file contains information on
+    :param connection:
+    :param sys_path:
+    :param cmd_prefix:
+    :return:
+    """
+    command = cmd_prefix + 'cat {{}}'.format(sys_path)
+    master = {}
+
+    try:
+        # do ->  cat /sys/class/net/bonding_masters
+        #         bond0 fabric
+        output, error = Client.exec_command(connection, command)
+
+        for master_line in output.split(str('\n')):
+            for bonding_device in master_line:
+                master[bonding_device] = {}
+
+                # Pull configuration from /proc/net/bonding/<bonding_device>
+
+                # do ->  cat cat /proc/net/bonding/<device>
+                #        Ethernet Channel Bonding Driver: v3.7.1 (April 27, 2011)
+                #
+                #        Bonding Mode: load balancing (round-robin)
+                #        MII Status: down
+                #        MII Polling Interval (ms): 0
+                #        Up Delay (ms): 0
+                #        Down Delay (ms): 0
+                # or
+                #       Ethernet Channel Bonding Driver: 2.6.1 (October 29, 2004)
+                #       Bonding Mode: load balancing (round-robin)
+                #       Currently Active Slave: eth0
+                #       MII Status: up
+                #       MII Polling Interval (ms): 1000
+                #       Up Delay (ms): 0
+                #       Down Delay (ms): 0
+                #
+                #       Slave Interface: eth1
+                #       MII Status: up
+                #       Link Failure Count: 1
+                #
+                #       Slave Interface: eth0
+                #       MII Status: up
+                #       Link Failure Count: 1
+                #
+                command = cmd_prefix + 'cat /proc/net/bonding/' + bonding_device
+
+                output, error = Client.exec_command(connection, command)
+
+                slave_name = None
+
+                for device_line in output.split(str('\n')):
+                    if len(device_line.strip()) == 0:
+                        slave_name = None
+                        continue
+
+                    fields = device_line.split(':')
+
+                    if len(fields) == 2:
+                        if fields[0].strip().lower() == 'slave interface':
+                            slave_name = fields[0].strip()
+                            master[bonding_device][slave_name] = {}
+                            master[bonding_device][slave_name][fields[0].strip()] = fields[1].strip()
+
+                        elif slave_name is not None:
+                            master[bonding_device][slave_name][fields[0].strip()] = fields[1].strip()
+                        else:
+                            master[bonding_device][fields[0].strip()] = fields[1].strip()
+
+    except Exception as e:
+        logging.exception('Client._get_device_detail')
+
+    logging.debug('_get_device_detail: detail:\n{}'.format(pprint.PrettyPrinter(indent=2).pformat(master)))
+
+    return _get_bonding_master_detail
 
 
 def _get_device_detail(connection, sys_path, cmd_prefix=""):
