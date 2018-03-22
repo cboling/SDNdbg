661257a642e239c042c365e8aace285892fd46c1
Chip Boling <chip@bcsw.net>
2018-03-21 19:45:47 -0500

    Update date in copyright

---
+++ env.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+#
+###############################################################################################
+#
+# Copyright (c) 2015 - Present.  Boling Consulting Solutions, BCSW.net
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+###############################################################################################
+# sourcing this file is needed to make local development and integration testing work
+export SDNDBG_BASE=${PWD}
+
+# load local python virtualenv if exists, otherwise create it
+VENVDIR="venv-$(uname -s | tr '[:upper:]' '[:lower:]')"
+if [ ! -e "${VENVDIR}/.built" ]; then
+    echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    echo "Initializing OS-appropriate virtual env."
+    echo "This will take a few minutes."
+    echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    make venv
+fi
+. ${VENVDIR}/bin/activate
+
+# add top-level SDNdbg dir to pythonpath
+export PYTHONPATH=${SDNDBG_BASE}/${VENVDIR}/lib/python2.7/site-packages:${PYTHONPATH}:${SDNDBG_BASE}
+
+# assign DOCKER_HOST_IP to be the main ip address of this host
+export DOCKER_HOST_IP=$(python support/utils/nethelpers.py)
+
+export PATH=${SDNDBG_BASE}/bin:${PATH}
